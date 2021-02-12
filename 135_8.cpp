0bf5738fe4da10c37f4e1e7d47b0ae3c428db79e
LotovIgor <55570083+LotovIgor@users.noreply.github.com>
2021-02-12 19:43:06 +0700

    Create 135_8.cpp

---
+++ 135_8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n, a;
+    cin >> n;
+    int m[n];
+    float c, f = (1 + sqrt(5)) / 2;
+    for (int i = 0; i <= n - 1; i++){
+        cin >> m[i];
+    }
+    for (int i = 0; i<= n - 1; i++){
+        int a0 = 1, a1 = 1;
+        for (int j = 1; j <= m[i]; j++){
+            if (a1 == m[i]){
+                cout << m[i] << endl;
+                break;
+            }
+            a = a0;
+            a0 = a1;
+            a1 = a + a0;
+            if (a > m[i])
+            break;
+        }
+    }
+}

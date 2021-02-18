d3ac9a751748dba72bc357e9a3ce8fd8b2e1c31d
LotovIgor <55570083+LotovIgor@users.noreply.github.com>
2021-02-18 22:42:52 +0700

    Create 135_10_6.cpp

---
+++ 135_10_6.cpp
@@ -0,0 +1,49 @@
+#include <gtk/gtk.h>
+#include <iostream>
+#include <math.h>
+
+void on_draw_event (GtkWidget *widget, cairo_t *cr, gpointer data)
+{
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    cairo_move_to(cr, 100, 300);
+    cairo_line_to(cr, 100, 200);
+    cairo_line_to(cr, 200, 200);
+    cairo_line_to(cr, 200, 300);
+    cairo_line_to(cr, 100, 300);
+    for (int i = 1; i < 20; i++){
+        cairo_move_to(cr, 100, 300 - i*5);
+        cairo_line_to(cr, 200, 300 - i*5);
+    }
+    cairo_stroke(cr);
+}
+ 
+
+int main( int argc, char *argv[])
+{
+   
+    gtk_init(&argc, &argv);
+ 
+    GtkWidget *window;
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+    
+    GtkWidget *vbox;
+    vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 1);
+    gtk_container_add(GTK_CONTAINER (window), vbox);
+ 
+    GtkWidget *drawing_area;
+    drawing_area = gtk_drawing_area_new();
+
+    gtk_box_pack_start (GTK_BOX(vbox), drawing_area, TRUE, TRUE, 10);
+
+    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(on_draw_event), NULL);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+ 
+}
+
